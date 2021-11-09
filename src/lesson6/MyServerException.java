9d4d1ceddbc354e6569be6dfc17811dea36d55eb
cheshrst <seva1999@yandex.lru>
2021-11-09 16:32:01 +0300

    09.11.2021

---
+++ src/lesson6/MyServerException.java
@@ -0,0 +1,11 @@
+package lesson6;
+
+public class MyServerException extends RuntimeException {
+    public MyServerException(String message) {
+        super(message);
+    }
+
+    public MyServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
